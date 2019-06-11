41d1ca05e2242567755ccb53a85434c6691c9a84
Anita <miringanita@gmail.com>
2019-06-11 17:44:15 +0200

    wireframe rectangle

---
+++ Wireframe_rectangle/Shader1.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader1
+{
+public:
+	unsigned int ID;
+
+	Shader1(const char* vertexPath, const char* fragmentPath);
+
+	void use();
+
+	void setBool(const std::string& name, bool value) const;
+
+	void setInt(const std::string& name, int value) const;
+
+	void setFloat(const std::string& name, float value) const;
+private:
+
+	void checkCompileErrors(unsigned int shader, std::string type);
+};
+
+#endif
+
